47dde4bd4f56c84333a9f7bfbb22168b64536467
Sara-H-Shaikh <87994090+Sara-H-Shaikh@users.noreply.github.com>
2021-08-28 13:11:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,101 @@
+// Create a reference for the canvas
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height)
+}
+
+//Write a code to grab the key-pressed event
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+
+		//write a code to check the type of key pressed
+	 
+	  if ((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+    {
+	  aplhabetkey();
+		document.getElementById("d1").innerHTML="You pressed Alphabet key";
+		console.log("alphabetKey");
+    }
+
+	 if	((keyPressed >=48 && keyPressed<=57))
+	{
+	 numberkey();
+	   document.getElementById("d1").innerHTML="You pressed Number key";
+	   console.log("numberKey");
+	}
+
+	 if	((keyPressed >=37 && keyPressed<=40))
+	{
+	 arrowkey();
+	   document.getElementById("d1").innerHTML="You pressed Arrow key";
+	   console.log("ArrKey");
+	}
+
+	 if	((keyPressed >= 17 && keyPressed<=27))
+	{
+	 specialkey();
+	   document.getElementById("d1").innerHTML="You pressed Spacial key";
+	   console.log("spKey");
+	}
+
+	 if	((keyPressed >= 1 && keyPressed<= 16)|| (keyPressed >= 28 && keyPressed<=36)|| (keyPressed >=41 && keyPressed<=47)|| (keyPressed >=58 && keyPressed<=64)|| (keyPressed >= 91 && keyPressed<=96))
+	{
+	 otherkey();
+	   document.getElementById("d1").innerHTML="You pressed Other key";
+	   console.log("otherKey");
+	}
+       
+
+}
+
+function aplhabetkey()
+{
+	//upload respective image with the message.
+	img_image = "Alpkey.png";
+	add();
+
+}
+function numberkey()
+{
+	img_image = "numkey.png";
+	add();
+}
+function arrowkey()
+{
+	img_image = "Arrkey.png";
+	add();
+}
+function specialkey()
+{
+	img_image = "spkey.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
